b49f042ae092d24d6abd4870b4e4d42550a2b8d6
SandroBeatz <amikhailov@intelliants.com>
2024-06-04 10:36:58 +0600

    added shadcn-nuxt

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -1,5 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import { defineNuxtConfig } from 'nuxt/config'
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  modules: ["@nuxtjs/tailwindcss", "shadcn-nuxt"],
+  devtools: { enabled: true },
+  css: ['@/assets/scss/main.scss']
 })
---
+++ client/pages/index.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import Button from "@/components/ui/button/Button.vue"
+
+</script>
+
+<template>
+  <div class="s-page">
+    <h1>Home page</h1>
+    <Button variant="secondary">Sandro Beatz</Button>
+  </div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
